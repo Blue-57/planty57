ef9a4edb7b6953be908d61c850f5677c2f252525
Blue <alexpaquin572@outlook.fr>
2023-12-01 18:07:34 +0100

    modification des pages

--- astra-child/functions.php
+++ astra-child/functions.php
@@ -4,5 +4,9 @@
 add_action('wp_enqueue_scripts', 'wpchild_enqueue_styles');
 function wpchild_enqueue_styles()
 {
-    wp_enqueue_style('wpm-tm-beans-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('wpm-astra-style', get_stylesheet_directory_uri() . '/style.css');
+    wp_enqueue_style('wpm-astra-theme', get_stylesheet_directory_uri() . '/theme.css'); //fichier css pour modification
 }
+
+
+?>
